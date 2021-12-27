67270673ce43c338da235c8298f026d7f345a82a
kelcho spense <kelchospense88@gmail.com>
2021-12-27 22:09:48 +0300

    headers

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ useEffect(() => {
  const getMyNfts = async () => {
     const openseaData = await axios.get(
       'https://testnets-api.opensea.io/assets?asset_contract_address=0x8c0dbDc089215080A4753371ECE99d01C2D63eB3&order_direction=asc'
-    )
+      
+      ,{ headers: {"Access-Control-Allow-Origin" :  "*"}})
    // console.log(openseaData.data.assets);
       setPunkListData(openseaData.data.assets) //saves fetched data to punklist
    }
